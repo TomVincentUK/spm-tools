3b9d15cf84517a1874b232a089a52980590cade4
Tom Vincent <TomVincentCode@gmail.com>
2022-08-27 18:45:23 +0100

    Added versioneer

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="spm-tools",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Tom Vincent",
+    author_email="TomVincentCode@gmail.com",
+    description="A package to process and analyse scanning probe microscopy data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=requirements,
+)
--- spm_tools/__init__.py
+++ spm_tools/__init__.py
@@ -1,3 +1,5 @@
-from . import example_data, profile, level, scanline, io
+from . import example_data, profile, level, scanline, io, _version
 
 __all__ = [example_data, profile, level, scanline, io]
+
+__version__ = _version.get_versions()["version"]
